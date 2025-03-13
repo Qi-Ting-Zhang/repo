343be2ea9754204eb7ee183c64833bf0ab2e74a1
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-13 14:35:04 +0800

    更新 hb180.js

--- Scripts/hb180.js
+++ Scripts/hb180.js
@@ -8,15 +8,15 @@ if (body) {
     if (obj && obj.singer) {
       const songCombinations = [
         {
-            song_name: "恭喜发财，大吉大利",
-            song_singer: " ",
-            cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__16e6b048-a625-481f-ac4f-174642e8051f_1741840724601.jpg"
-          }
+          song_name: "恭喜发财，大吉大利",
+          song_singer: " ",
+          cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__16e6b048-a625-481f-ac4f-174642e8051f_1741840724601.jpg"
+        }
       ];
 
-      
+      let randomCombination = songCombinations[0];  // 使用第一个歌曲组合
 
-      obj.title = randomCombination.song_name; 
+      obj.title = randomCombination.song_name;
       obj.singer = randomCombination.song_singer;
       obj.cover = randomCombination.cover;
 
