b7c67f8cd49c7e8debbb6cb70afd5c990e96685a
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-13 15:41:53 +0800

    更新 hb.js

--- Scripts/hb.js
+++ Scripts/hb.js
@@ -1,8 +1,8 @@
 [rewrite_local]
 //misaka点歌
 ^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js
-//黄白178点歌
-^https://api.xingzhige.com/.*$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js
+//黄白178
+^https?:\/\/\^https://api\.xingzhige\.com/\.\*\$ url script-response-body https://raw\.githubusercontent\.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178\.js$
 //黄白180点歌
 ^https://www.hhlqilongzhu.cn/.*$ -url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js
 
