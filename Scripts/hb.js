d33b6b1c4f6f7cd7dc9556e7948576b2c3726994
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-13 16:31:32 +0800

    更新 hb.js

--- Scripts/hb.js
+++ Scripts/hb.js
@@ -2,9 +2,8 @@
 //misaka点歌
 ^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js
 //黄白178
-^https://api.xingzhige.com/.*$ -url script-response-body https://raw\.githubusercontent\.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js
-//黄白180点歌
-^https://www.hhlqilongzhu.cn/.*$ -url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js
-
+^https:\/\/api\.xingzhige.\com/.* url script-response-body https://raw\.githubusercontent\.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js
+//黄白180
+^https:\/\/www\.hhlqilongzhu\.cn/.* url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js
 [mitm] 
 hostname = api.xingzhige.com , www.hhlqilongzhu.cn , api.dragonlongzhu.cn
