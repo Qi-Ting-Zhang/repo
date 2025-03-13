d33f6c445bcc1e291c3387b36eb24be9f7de6366
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-13 14:35:34 +0800

    更新 mkqqyy.js

--- Scripts/mkqqyy.js
+++ Scripts/mkqqyy.js
@@ -5,22 +5,22 @@ if (body) {
     if (obj && obj.data) {
       const songCombinations = [
         {
-            song_name: "恭喜发财，大吉大利",
-            song_singer: " ",
-            cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__16e6b048-a625-481f-ac4f-174642e8051f_1741840724601.jpg"
-          }
+          song_name: "恭喜发财，大吉大利",
+          song_singer: " ",
+          cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__16e6b048-a625-481f-ac4f-174642e8051f_1741840724601.jpg"
+        }
       ];
 
+      let randomCombination = songCombinations[0];  // 使用第一个歌曲组合
 
       obj.data.song_name = randomCombination.song_name;
       obj.data.song_singer = randomCombination.song_singer;
       obj.data.cover = randomCombination.cover;
-
     }
-    $done({body: JSON.stringify(obj)});
+    $done({ body: JSON.stringify(obj) });
   } catch (e) {
     console.log("解析失败by胜天半子:", e);
-    $done({body});
+    $done({ body });
   }
 } else {
   $done({});
