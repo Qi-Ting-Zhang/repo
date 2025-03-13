3284c8007f7fb40d31aa19c0f5026b5411ce572f
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-13 16:42:28 +0800

    更新 hb178.js

--- Scripts/hb178.js
+++ Scripts/hb178.js
@@ -1,27 +1,22 @@
-let body = $response.body;  
-let headers = $response.headers;  
+let body = $response.body;
+let headers = $response.headers;
 
 if (body) {
   try {
     let obj = JSON.parse(body);
 
     if (obj && obj.data) {
-      const songCombinations = [
-        {
-          song_name: "恭喜发财，大吉大利",
-          song_singer: " ",
-          cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__16e6b048-a625-481f-ac4f-174642e8051f_1741840724601.jpg"
-        }
-      ];
+      const songCombination = {
+        song_name: "恭喜发财，大吉大利",
+        song_singer: " ",
+        cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__16e6b048-a625-481f-ac4f-174642e8051f_1741840724601.jpg"
+      };
 
-      let randomCombination = songCombinations[0];  // 使用第一个歌曲组合
-
-      obj.data.songname = randomCombination.song_name;
-      obj.data.name = randomCombination.song_singer;
-      obj.data.cover = randomCombination.cover;
+      obj.data.songname = songCombination.song_name;
+      obj.data.name = songCombination.song_singer;
+      obj.data.cover = songCombination.cover;
 
       let updatedBody = JSON.stringify(obj);
-
       $done({ body: updatedBody, headers: headers });
 
     } else {
@@ -29,7 +24,7 @@ if (body) {
     }
 
   } catch (e) {
-    console.log("by贾队长:", e);
+    console.log("Error:", e);
     $done({ body: body, headers: headers });
   }
 } else {
