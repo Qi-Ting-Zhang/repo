f7c309e3d091b19a4f40e21c2cbdfb8d4f362d16
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 22:15:25 +0800

    更新 hb.js

--- Scripts/hb.js
+++ Scripts/hb.js
@@ -4,6 +4,8 @@
 //黄白178
 ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js
 //黄白180
-^https?:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1 url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js
+//^https?:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1 url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js
+[rewrite_local]
+^https://www.hhlqilongzhu.cn/.*$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js
 [mitm] 
 hostname = api.xingzhige.com , www.hhlqilongzhu.cn , api.dragonlongzhu.cn
