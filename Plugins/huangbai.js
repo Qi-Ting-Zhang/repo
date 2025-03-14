c2c4fdeb71216c7cde6eba19df45f37ff4bf8d8a
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 20:14:49 +0800

    更新 huangbai.js

--- Plugins/huangbai.js
+++ Plugins/huangbai.js
@@ -3,7 +3,7 @@
  #!openUrl= https://qi-ting-zhang.github.io/sileo/
  #!author=祁厅长[https://h5.clewm.net/?url=qr61.cn%2FoMuCi0%2FqHe9QuO]
  #!homepage=https://github.com/Qi-Ting-Zhang/repo
- #!icon=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Icons/dg.png
+ #!icon=http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__e4ce1ddd-821c-4264-85fd-02059d93efbb_1741954438747.jpg
 
 [Script]
 
@@ -14,4 +14,4 @@ http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.
 http-response ^https:\/\/api\.dragonlongzhu\.cn\/.* script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js, requires-body=true, timeout=60, tag=misaka
 
 [MitM]
- hostname=www.hhlqilongzhu.cn , hostname=api.xingzhige.com , hostname=api.dragonlongzhu.cn
+api.xingzhige.com, www.hhlqilongzhu.cn, api.dragonlongzhu.cn
