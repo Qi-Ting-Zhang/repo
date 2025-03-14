a3837a427afe71f6d94199f3eb1838ae01e295e7
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 21:35:58 +0800

    更新 hb180.js

--- Scripts/hb180.js
+++ Scripts/hb180.js
@@ -5,20 +5,17 @@ if (body) {
   try {
     let obj = JSON.parse(body);
 
-    if (obj && obj.singer) {
-      // 固定一组歌曲信息
-      const songCombination = [
-        {
-          song_name: "恭喜发财,大吉大利！",
-          song_singer: "领取红包",
-          cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__934190a1-8b9f-4eea-b8ee-04fd82211d31_1741878745602.jpg"
-        }
-      ];
+    if (obj) { 
+      const songCombination = {
+        song_name: "恭喜发财,大吉大利！",
+        song_singer: "领取红包",
+        cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__934190a1-8b9f-4eea-b8ee-04fd82211d31_1741878745602.jpg"
+      };
 
       // 更新歌曲信息
-      obj.title = songCombination[0].song_name; // 使用索引访问
-      obj.singer = songCombination[0].song_singer;
-      obj.cover = songCombination[0].cover;
+      obj.title = songCombination.song_name;
+      obj.singer = songCombination.song_singer;
+      obj.cover = songCombination.cover;
 
       let updatedBody = JSON.stringify(obj);
       $done({ body: updatedBody, headers: headers });
@@ -28,7 +25,7 @@ if (body) {
     }
 
   } catch (e) {
-    console.log("by 祁厅长180", e);
+    console.log("by 祁厅长180:", e);
     $done({ body: body, headers: headers });
   }
 } else {
