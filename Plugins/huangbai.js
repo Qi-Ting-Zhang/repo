171e4b463319236c74128365113d22a30942030e
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 14:06:16 +0800

    更新 huangbai.js

--- Plugins/huangbai.js
+++ Plugins/huangbai.js
@@ -1,7 +1,10 @@
-#!name=Misaka点歌
-#!desc=通过Misaka点歌API提供歌曲推荐服务。
-#!openUrl=https://github.com/Qi-Ting-Zhang/repo
+#!name=黄白misaka点歌
+#!desc=通过接口提供网易云{黄白}qq音乐{misaka}点歌服务。
+#!openUrl= https://qi-ting-zhang.github.io/sileo/
 #!author=祁厅长
+#!homepage=https://github.com/Qi-Ting-Zhang/repo
+#!icon=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Icons/hb180_icon.png
+
 
 [Rule]
 AND,((DOMAIN-SUFFIX,dragonlongzhu.cn))
@@ -9,27 +12,11 @@ AND,((DOMAIN-SUFFIX,dragonlongzhu.cn))
 [Rewrite]
 ^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js,tag=Misaka点歌
 
-[MitM]
-hostname=api.dragonlongzhu.cn
-
-#!name=黄白178
-#!desc=通过黄白178接口提供网易云点歌服务。
-#!openUrl=https://github.com/Qi-Ting-Zhang/repo
-#!author=Qi-Ting-Zhang
-
 [Rule]
 AND,((DOMAIN-SUFFIX,xingzhige.com))
 
 [Rewrite]
 ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js,tag=黄白178
-
-[MitM]
-hostname=api.xingzhige.com
-
-#!name=黄白180
-#!desc=通过黄白180接口提供网易云点歌服务。
-#!openUrl=https://github.com/Qi-Ting-Zhang/repo
-#!author=Qi-Ting-Zhang
 
 [Rule]
 AND,((DOMAIN-SUFFIX,hhlqilongzhu.cn))
@@ -38,4 +25,4 @@ AND,((DOMAIN-SUFFIX,hhlqilongzhu.cn))
 ^https?:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1 url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js,tag=黄白180
 
 [MitM]
-hostname=www.hhlqilongzhu.cn
+hostname=www.hhlqilongzhu.cn , hostname=api.xingzhige.com , hostname=api.dragonlongzhu.cn
