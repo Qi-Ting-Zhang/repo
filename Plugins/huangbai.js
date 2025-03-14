dbdf48f999f47a34c631e51cbbd3df3490d130ec
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 16:43:08 +0800

    更新 huangbai.js

--- Plugins/huangbai.js
+++ Plugins/huangbai.js
@@ -1,16 +1,5 @@
-#!name=点歌
-#!desc=黄白选网易，misaka选qq
-#!openUrl= https://qi-ting-zhang.github.io/sileo/
-#!author=祁厅长[https://h5.clewm.net/?url=qr61.cn%2FoMuCi0%2FqHe9QuO]
-#!homepage=https://github.com/Qi-Ting-Zhang/repo
-#!icon=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Icons/dg.png
+http-response ^https:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1&type=json$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, timeout=60, tag=黄白
 
+http-response ^https:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1&type=json$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, timeout=60, tag=黄白
 
-[Script]
-http-response ^https?:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1 url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, timeout=60, tag=黄白
-
-http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, timeout=60, tag=黄白
-
-http-response ^https:\/\/api\.dragonlongzhu\.cn\/api\/dg_shenmiMusic_SQ\.php\?msg=[^&]+&n=1&type=json$ -url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, timeout=60, tag=misaka
-[MitM]
-hostname=www.hhlqilongzhu.cn , hostname=api.xingzhige.com , hostname=api.dragonlongzhu.cn
+http-response ^https:\/\/api\.dragonlongzhu\.cn\/api\/dg_shenmiMusic_SQ\.php\?msg=[^&]+&n=1&type=json$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, timeout=60, tag=misaka
