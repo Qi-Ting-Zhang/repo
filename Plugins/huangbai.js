a283fa1531d4409fb9901b7055ff4d042066d46a
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 22:20:47 +0800

    更新 huangbai.js

--- Plugins/huangbai.js
+++ Plugins/huangbai.js
@@ -7,7 +7,7 @@
 
 [Script]
 
-http-response ^https?:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, requires-body=true, timeout=60, tag=黄白
+http-response ^https://www.hhlqilongzhu.cn/.*$ script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, requires-body=true, timeout=60, tag=黄白180
 
 http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, requires-body=true, timeout=60, tag=黄白
 
