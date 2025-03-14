6a25363538b828959adac13b196f0db4302959da
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 20:32:11 +0800

    更新 huangbai.js

--- Plugins/huangbai.js
+++ Plugins/huangbai.js
@@ -14,4 +14,4 @@ http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.
 http-response ^https:\/\/api\.dragonlongzhu\.cn\/.* script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js, requires-body=true, timeout=60, tag=misaka
 
 [MitM]
-api.xingzhige.com, www.hhlqilongzhu.cn, api.dragonlongzhu.cn
+hostname = api.xingzhige.com , www.hhlqilongzhu.cn , api.dragonlongzhu.cn
