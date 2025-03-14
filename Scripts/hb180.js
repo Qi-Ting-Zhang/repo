3b553cfffefd78f11f22710c64eb9c3fdb363d70
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 21:22:12 +0800

    更新 hb180.js

--- Scripts/hb180.js
+++ Scripts/hb180.js
@@ -6,19 +6,19 @@ if (body) {
     let obj = JSON.parse(body);
 
     if (obj && obj.singer) {
-      // 固定一组歌曲信息
+      
       const songCombination = [
         {
-        song_name: "恭喜发财,大吉大利！",
-        song_singer: "领取红包",
-        cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__934190a1-8b9f-4eea-b8ee-04fd82211d31_1741878745602.jpg"
-      }
+          song_name: "恭喜发财,大吉大利！",
+          song_singer: "领取红包",
+          cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__934190a1-8b9f-4eea-b8ee-04fd82211d31_1741878745602.jpg"
+        }
       ];
 
       // 更新歌曲信息
-      obj.title = songCombination.song_name;
-      obj.singer = songCombination.song_singer;
-      obj.cover = songCombination.cover;
+      obj.title = songCombination[0].song_name; // 使用索引访问
+      obj.singer = songCombination[0].song_singer; // 使用索引访问
+      obj.cover = songCombination[0].cover; // 使用索引访问
 
       let updatedBody = JSON.stringify(obj);
 
