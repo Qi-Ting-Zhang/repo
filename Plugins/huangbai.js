0f46553e504b13b30ad7c7caf1fef2fac2ed2db2
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 15:58:51 +0800

    更新 huangbai.js

--- Plugins/huangbai.js
+++ Plugins/huangbai.js
@@ -1,7 +1,7 @@
 #!name=黄白misaka点歌
 #!desc=通过接口提供网易云{黄白}qq音乐{misaka}点歌服务。
 #!openUrl= https://qi-ting-zhang.github.io/sileo/
-#!author=祁厅长
+#!author=祁厅长[https://h5.clewm.net/?url=qr61.cn%2FoMuCi0%2FqHe9QuO]
 #!homepage=https://github.com/Qi-Ting-Zhang/repo
 #!icon=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Icons/hb180_icon.png
 
@@ -12,6 +12,6 @@ http-response ^https?:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name
 http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js,tag=黄白178
 
 http-response ^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js,tag=Misaka点歌
-
+http-response ^https:\/\/api\.dragonlongzhu\.cn\/api\/dg_shenmiMusic_SQ\.php\?msg=[^&]+&n=1&type=json$ -url script-response-body mkqqyy.js
 [MitM]
 hostname=www.hhlqilongzhu.cn , hostname=api.xingzhige.com , hostname=api.dragonlongzhu.cn
