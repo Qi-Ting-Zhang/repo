a10a00f4c01b797d46b29ca81e88f0d0f77443e9
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 20:02:13 +0800

    更新 huangbai.js

--- Plugins/huangbai.js
+++ Plugins/huangbai.js
@@ -1,17 +1,17 @@
- #!name=点歌
+ #!name=点歌卡片
  #!desc=黄白选网易，misaka选qq
  #!openUrl= https://qi-ting-zhang.github.io/sileo/
  #!author=祁厅长[https://h5.clewm.net/?url=qr61.cn%2FoMuCi0%2FqHe9QuO]
  #!homepage=https://github.com/Qi-Ting-Zhang/repo
- #!icon=http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__6de99351-7963-4361-968c-079e21edc0d6_1741944910772.jpg
+ #!icon=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Icons/dg.png
 
 [Script]
 
-http-response ^https:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1&type=json$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, timeout=60, tag=黄白
+http-response ^https?:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, requires-body=true, timeout=60, tag=黄白
 
-http-response ^https:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1&type=json$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, timeout=60, tag=黄白
+http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, requires-body=true, timeout=60, tag=黄白
 
-http-response ^https:\/\/api\.dragonlongzhu\.cn\/api\/dg_shenmiMusic_SQ\.php\?msg=[^&]+&n=1&type=json$ -url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js, timeout=60, tag=misaka
+http-response ^https:\/\/api\.dragonlongzhu\.cn\/.* script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/mkqqyy.js, requires-body=true, timeout=60, tag=misaka
 
 [MitM]
-hostname=www.hhlqilongzhu.cn , hostname=api.xingzhige.com , hostname=api.dragonlongzhu.cn
+ hostname=www.hhlqilongzhu.cn , hostname=api.xingzhige.com , hostname=api.dragonlongzhu.cn
