88660ca21ccab7ae817e7512483288649399fa44
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 21:28:27 +0800

    更新 hb180.js

--- Scripts/hb180.js
+++ Scripts/hb180.js
@@ -6,7 +6,7 @@ if (body) {
     let obj = JSON.parse(body);
 
     if (obj && obj.singer) {
-      
+      // 固定一组歌曲信息
       const songCombination = [
         {
           song_name: "恭喜发财,大吉大利！",
@@ -17,11 +17,10 @@ if (body) {
 
       // 更新歌曲信息
       obj.title = songCombination[0].song_name; // 使用索引访问
-      obj.singer = songCombination[0].song_singer; // 使用索引访问
-      obj.cover = songCombination[0].cover; // 使用索引访问
+      obj.singer = songCombination[0].song_singer;
+      obj.cover = songCombination[0].cover;
 
       let updatedBody = JSON.stringify(obj);
-
       $done({ body: updatedBody, headers: headers });
 
     } else {
