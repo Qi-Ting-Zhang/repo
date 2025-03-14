aa3c6551bcd15da8ba79dca0164aba930f16d41c
祁厅长 <164759932+Qi-Ting-Zhang@users.noreply.github.com>
2025-03-14 17:05:17 +0800

    更新 huangbai.js

--- Plugins/huangbai.js
+++ Plugins/huangbai.js
@@ -7,7 +7,7 @@
 
 [Script]
 
-http-response ^https:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1&type=json$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/180.js, timeout=60, tag=黄白
+http-response ^https:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1&type=json$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb180.js, timeout=60, tag=黄白
 
 http-response ^https:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1&type=json$ url script-response-body https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, script-path=https://raw.githubusercontent.com/Qi-Ting-Zhang/repo/refs/heads/master/Scripts/hb178.js, timeout=60, tag=黄白
 
